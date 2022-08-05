69a1ea1c8124c6191b1b223110d0585a1b85df76
sohaib <sohaib.kabeer@avanzasolutions.com>
2022-08-06 01:56:40 +0500

    adding in Constatns.swift file again and removing them from .gitignore for time being

---
+++ youtube_likeApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  youtube_likeApp
+//
+//  Created by Avanza on 8/5/22.
+//
+
+import Foundation
+
+struct Constants{
+    
+    static var API_KEY = "AIzaSyDMI-bjmjJ8-mUbsJ9xLrLa27-l6kHuO28"
+    static var PLAYLIST_ID = "PLMRqhzcHGw1ZNevU17Xk8H-1NJk8n7_j-"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
