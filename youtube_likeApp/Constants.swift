d09e65ce8f0dc3ff9f2ed52c741e30b7cd4c9d8c
sohaib <sohaib.kabeer@avanzasolutions.com>
2022-08-10 09:51:51 +0500

    added Response Struct, Added a tableView to the storyboard, added a protocal in the model class

--- youtube_likeApp/Constants.swift
+++ youtube_likeApp/Constants.swift
@@ -12,4 +12,5 @@ struct Constants{
     static var API_KEY = "AIzaSyDMI-bjmjJ8-mUbsJ9xLrLa27-l6kHuO28"
     static var PLAYLIST_ID = "PLMRqhzcHGw1ZNevU17Xk8H-1NJk8n7_j-"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEOCELL_ID = "VideoCell"
 }
