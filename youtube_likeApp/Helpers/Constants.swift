b6c5255dddf7807eaa4a32043e0457c89a8bbf79
sohaib <sohaib.kabeer@avanzasolutions.com>
2022-08-12 23:06:20 +0500

    added segue to another viewcontroller to show the detailed view of the videos when taped in the table view.

--- youtube_likeApp/Helpers/Constants.swift
+++ youtube_likeApp/Helpers/Constants.swift
@@ -13,4 +13,5 @@ struct Constants{
     static var PLAYLIST_ID = "PLMRqhzcHGw1ZNevU17Xk8H-1NJk8n7_j-"
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
 }
